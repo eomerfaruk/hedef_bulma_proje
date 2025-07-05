8eb21de28786a15d49305e9f3180f69148b77810
eomerfaruk <eomerfaruk28@gmail.com>
2025-07-05 17:43:29 +0300

    duzeltme

--- hedef_tespit.py
+++ hedef_tespit.py
@@ -28,3 +28,25 @@ hedef_kordinatları = []
 for c in cnts:
     (x,y,w,h) = cv2.boundingRect(c)
     hedef_kordinatları.append((x,y,w,h))
+
+img = cv2.imread("3.png")
+
+img = cv2.cvtColor(img , cv2.COLOR_BGR2HSV)
+lower = np.array([0, 0, 0])      
+upper = np.array([123, 201,120 ]) 
+
+img = cv2.inRange(img , lower , upper)
+img  = cv2.dilate(img  ,None , iterations=2)
+
+cnts_daire  = cv2.findContours(img , cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+cnts_daire = imutils.grab_contours(cnts_daire)
+
+cnts_daire = [c for c in cnts_daire if cv2.contourArea(c) > 6400 ]
+(a,b)= img.shape[:2]
+
+matris = np.ones((a,b) , dtype= np.uint8)*255
+cnts_daire  = sorted (cnts_daire , key = cv2.contourArea , reverse = True)
+
+cnts_daire = [c for i,c in enumerate(cnts_daire) if i %2 == 0 ]
+
+
